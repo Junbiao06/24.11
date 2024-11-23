e594d66b9992c9ae33971ae1ad605f780f775e32
Junbiao06 <2675661022@qq.com>
2024-11-23 23:42:12 +0800

    Add files via upload

--- test1.c
+++ test1.c
@@ -1,14 +1,18 @@
-#include<stdio.h>
+#include <stdio.h>
+
 int main(void)
 {
-
-int a, b=0;
-
-
-       for ( a=0 ; a++<=2 ; ) ;
-
-
-              b += a;
-
-       printf("%d, %d\n",a,b);
+    int n = 5;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n - i; j++)
+        {
+            printf(" ");
+        }
+        for (int k = 1; k <= 2 * i - 1; k++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
 }
