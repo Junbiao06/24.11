6c929cbf26bac7d3ea062b5e3db91f3f6cbf9c31
Junbiao06 <2675661022@qq.com>
2024-11-25 22:13:27 +0800

    Add files via upload

--- muke1.c
+++ muke1.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
+#include<math.h>
+#define pi 3.14
+
+double surface(double r, double h);
+double volume(double r, double h);
+
 int main(void)
 {
-    double a;
-    scanf("%lf", &a);
-    double b = 5 * (a - 32) / 9;
-    printf("Celsius=%lf", b);
+    double r, h, s, v;
+    scanf("%lf%lf", &r, &h);
+    s = surface(r, h);
+    v = volume(r, h);
+    printf("radius=%lf, height=%lf, Area of surface=%lf, Volume=%lf\n", r, h, s, v);
+}
+
+double surface(double r, double h)
+{
+    double sur = pi * r * sqrt(h*h+r*r)  + pi * r * r;
+    return sur;
+}
+
+double volume(double r, double h)
+{
+    double vol = pi * r * r * h / 3;
+    return vol;
 }
